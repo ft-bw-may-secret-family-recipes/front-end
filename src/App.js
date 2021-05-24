f8868e247a0c838a39b7cf0991ccc018938011c5
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-05-24 15:19:06 -0600

    adding new recipe component file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Recipe from './components/RecipeCard';
+import Newrecipe from './components/Newrecipe';
 
 function App() {
   return (
     <div className="App">
         <Recipe/>
+        <Newrecipe/>
     </div>
   );
 }
