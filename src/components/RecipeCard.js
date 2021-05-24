2c0f3c1cd0ba1a539fbc7a95d94b5c1a0df6ad56
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-05-24 15:10:38 -0600

    testing styles and sizing

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Div = styled.div`
+    background-color: #A1978F;
+    width: 50%;
+    margin: auto;
+`;
 function Recipe() {
     return(
-        <div>
+        <Div>
             <h3>Title: test</h3>
             <h4>Source: test</h4>
             <p>Ingrediants: test</p>
             <p>Insctructions: test</p>
             <h4>Catagory: test</h4>
-        </div>
+        </Div>
     )
 }
 
