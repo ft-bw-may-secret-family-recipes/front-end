aac1c462c57ac3137b07ed78e75f58232ceec6a5
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-05-24 15:19:45 -0600

    adding newrecipe.js

---
+++ src/components/Newrecipe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NewRecipe() {
+    return(
+        <form>
+            <lable>
+                Title:
+                <input type='text'></input>
+            </lable>
+        </form>
+    )
+}
+
+export default NewRecipe
