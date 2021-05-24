b3f564985cc378194427a72492028a1df49c9058
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-05-24 15:43:55 -0600

    messing with spacing

--- src/components/Newrecipe.js
+++ src/components/Newrecipe.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Form = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: space-evenly;
     width: 45%;
     margin: auto;
 `;
