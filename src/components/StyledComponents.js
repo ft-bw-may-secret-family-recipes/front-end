914a8049f78659921ed9f1ce4f22dfaa9040170b
John Laubscher <jlmemphiscommanders@gmail.com>
2021-05-27 15:40:46 -0500

    styling for home, signup, login pages

--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -1,13 +1,24 @@
 import styled from "styled-components";
+import logInImg from "./PastaLogIn.jpg";
+import signUpImg from "./Cake-signup.jpg";
 
 const StyledPage = styled.div`
   #HomePage {
     background-image: url("https://images.unsplash.com/photo-1459789034005-ba29c5783491?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1700&q=80");
+    min-height: 100%;
+    min-width: 1024;
+    width: 100%;
+    height: auto;
+    top: 0;
+    left: 0;
+    max-width: 100%;
+    max-height: 100%;
+    position: fixed;
+
     background-repeat: no-repeat;
     background-position: center;
-    /* background-size: cover; */
-    height: 700px;
-    width: 100%;
+    /* background-size: contain; */
+    /* object-fit: contain; */
   }
 
   h1 {
@@ -25,7 +36,7 @@ const StyledPage = styled.div`
     text-shadow: 1px 1px black;
     -webkit-text-stroke-width: 1px;
     -webkit-text-stroke-color: black;
-    font-size: 2rem;
+    font-size: 2.5rem;
     /* border: 1px ridge black;
         margin: 5px 5px; */
   }
@@ -50,35 +61,56 @@ const StyledPage = styled.div`
 
   button {
     background-color: transparent;
+    opacity: 0.8;
 
     border-radius: 8px;
     color: black;
-    /* -webkit-text-stroke-width: 0.2px;
-    -webkit-text-stroke-color: white; */
-    text-shadow: 0.5px 0.5px black;
-    font-size: 1.7rem;
+    -webkit-text-stroke-width: 0.2px;
+    -webkit-text-stroke-color: white;
+    text-shadow: 0.5px 0.5px white;
+    font-size: 2.5rem;
     margin: 4px;
     font-weight: bold;
   }
   #logInForm {
-    background-image: url
-      (
-        "https://images.unsplash.com/photo-1481070555726-e2fe8357725c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"
-      );
+    background-image: url(${logInImg});
+    min-height: 100%;
+    min-width: 1024;
+    width: 100%;
+    height: auto;
+    top: 0;
+    left: 0;
+    max-width: 100%;
+    max-height: 100%;
+    position: fixed;
+
     background-repeat: no-repeat;
     background-position: center;
-    /* background-size: cover; */
-    height: 700px;
-    width: 100%;
+    background-size: cover;
+  }
+  #logInForm button {
+    color: white;
+    text-shadow: 1px 1px black;
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: black;
+    font-size: 2.3rem;
   }
 
   #signUpForm {
-    background-image: url("https://cdn-cajkg.nitrocdn.com/ZJzLelPaQQUYcIzKvveTxTAioLgFVwpU/assets/static/optimized/rev-cd29551/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-768x1024.jpg.webp");
+    background-image: url(${signUpImg});
+    min-height: 100%;
+    min-width: 1024;
+    width: 100%;
+    height: auto;
+    top: 0;
+    left: 0;
+    max-width: 100%;
+    max-height: 100%;
+    position: fixed;
+
     background-repeat: no-repeat;
     background-position: center;
-    /* background-size: cover; */
-    height: 700px;
-    width: 100%;
+    background-size: cover;
   }
 `;
 
