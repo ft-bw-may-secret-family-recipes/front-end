53dd8a6d061a79424f6cd13848f022206e289a7f
John Laubscher <jlmemphiscommanders@gmail.com>
2021-05-27 01:44:12 -0500

    still working. Pic is showing up on sign up, but not log in. Form needs to be rearranged

--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -15,6 +15,7 @@ const StyledPage = styled.div`
     text-shadow: 2px 2px black;
     -webkit-text-stroke-width: 1px;
     -webkit-text-stroke-color: black;
+    align-items: center;
   }
 
   a {
@@ -27,19 +28,42 @@ const StyledPage = styled.div`
     /* border: 1px ridge black;
         margin: 5px 5px; */
   }
+  label {
+    text-decoration: none;
+    color: white;
+    text-shadow: 1px 1px black;
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: black;
+    font-size: 2.5rem;
+  }
   form {
     display: flex;
-    justify-content: space-around;
+    flex-direction: column;
+    align-items: center;
+    align-content: center;
+  }
+  .spacingDiv {
+    width: 30%;
   }
   #logInForm {
     background-image: url
       (
         "https://images.unsplash.com/photo-1481070555726-e2fe8357725c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"
       );
+    background-repeat: no-repeat;
+    background-position: center;
+    /* background-size: cover; */
+    height: 700px;
+    width: 100%;
   }
 
   #signUpForm {
-    background-image: url("https://images.unsplash.com/photo-1485921325833-c519f76c4927?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80");
+    background-image: url("https://images.unsplash.com/photo-1615126848430-d9a0c00d20bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80");
+    background-repeat: no-repeat;
+    background-position: center;
+    /* background-size: cover; */
+    height: 700px;
+    width: 100%;
   }
 `;
 
